84f8aa5d266a6dac93ee030958ff2cb34cf7c811
cjefferies7981 <151930408+cjefferies7981@users.noreply.github.com>
2023-12-12 15:39:39 -0500

    Updating Date Class
    Adding instance variables based on API return

--- Data.java
+++ Data.java
@@ -1,6 +1,12 @@
 public class Data
 {
    // Top-level class properties
-   public String flightStatus;
-   public String flightDate;
+   public String flight_date;
+   public String flight_status;
+   public String[] departure;
+   public String[] arrival;
+   public String[] airline;
+   public String[] flight;
+   public String aircraft;
+   public String live;
 }
